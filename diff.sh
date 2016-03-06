d53239a867df3890abd72d1d4f91f31015dd652a
danprince <danielprince@hotmail.co.uk>
2016-03-06 12:40:06 +0530

    supress git output for new remote

--- diff.sh
+++ diff.sh
@@ -4,10 +4,10 @@ upstream="git@github.com:danprince/ci-suggest.git"
 remotes=$(git remote show)
 
 # add this remote if it doesn't already exist
-git remote show | grep -q '^upstream$'
+git remote show | grep --quiet '^upstream$'
 remote_exists=$?
 if [[ $remote_exists -ne 0 ]] ; then
-  git remote add upstream $upstream
+  git remote add upstream $upstream --quiet
 fi
 
 # diff against upstream master
