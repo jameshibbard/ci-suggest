6efc6296d57fa26f11b7eae9439375b123e02073
danprince <danielprince@hotmail.co.uk>
2016-03-06 12:46:46 +0530

    apparently -q doesn't silence git

--- diff.sh
+++ diff.sh
@@ -4,10 +4,10 @@ upstream="git@github.com:danprince/ci-suggest.git"
 remotes=$(git remote show)
 
 # add this remote if it doesn't already exist
-git remote show | grep --quiet '^upstream$'
+git remote show | grep -q '^upstream$'
 remote_exists=$?
 if [[ $remote_exists -ne 0 ]] ; then
-  git remote add upstream $upstream --quiet
+  git remote add upstream $upstream > /dev/null
 fi
 
 # diff against upstream master
